2d4559421c2536ca884e5336e5e733a5a4db89ff
Cla Tschenett <design@clatschenett.com>
2016-10-16 19:03:31 +0200

    Getter, Setter and ToString added

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,38 @@
 public class Student {
-    public String name;
-    public int nummer;
+
+    private String name;
+    private int nummer;
+
+    //Getter und Setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int n) {
+        int alteNummer = nummer;
+        nummer = n;
+        if (!validateNummer()) {
+            nummer = alteNummer;
+        }
+    }
+
+    //ValidateNumber
+    public boolean validateNummer() {
+        return (nummer > 1000 && nummer <= 99999 && nummer % 2 != 0);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + nummer + ")";
+    }
+
 }
---
+++ src/TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+
+    public static void main (String[] args) {
+        Student studi1 = new Student();
+        studi1.setName("Karla Karlsson");
+        studi1.setNummer(12345);
+
+        System.out.println(studi1);
+    }
+}
